e03f7bf85ad313a3455d11ea46dbe4941c2f8014
aimystuff-bk <bk@aimystuff.net>
2024-03-24 03:08:27 -0400

    mongo prompts into its own file

---
+++ internal/mongo/prompts.go
@@ -0,0 +1,42 @@
+package mongo
+
+import (
+	"context"
+	"errors"
+
+	log "github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+
+	"github.com/stuff-ai/api/pkg/types"
+)
+
+func promptsCollection() *mongo.Collection {
+	return db().Collection("prompts")
+}
+
+func AddPrompt(ctx context.Context, prompt *types.Prompt) error {
+	_, err := promptsCollection().InsertOne(
+		ctx,
+		bson.D{{"title", prompt.Title}, {"prompt", prompt.Prompt}},
+	)
+	return err
+}
+
+func RandomPrompt(ctx context.Context) (*types.Prompt, error) {
+	filter := mongo.Pipeline{bson.D{{"$sample", bson.D{{"size", 1}}}}}
+	cur, err := promptsCollection().Aggregate(ctx, filter)
+	if err != nil {
+		log.WithError(err).Error("RandomPrompt: collection.Aggregate")
+		return nil, err
+	}
+	var results []*types.Prompt
+	if err = cur.All(ctx, &results); err != nil {
+		log.WithError(err).Error("RandomPrompt: cursor.Decode")
+		return nil, err
+	}
+	if len(results) == 0 {
+		return nil, errors.New("RandomPrompt: no results")
+	}
+	return results[0], nil
+}
