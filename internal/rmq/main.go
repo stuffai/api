949e1d63f40beb6d1642debd6c17644947ff761e
aimystuff-bk <bk@aimystuff.net>
2024-03-24 02:02:13 -0400

    refactor rmq to its own pkg

---
+++ internal/rmq/main.go
@@ -0,0 +1,46 @@
+package rmq
+
+import (
+	"context"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+var conn *amqp.Connection
+
+func init() {
+	var err error
+	if conn, err = amqp.Dial("amqp://guest:guest@192.168.63.29:5672/"); err != nil {
+		panic("failled to initialize amqp: " + err.Error())
+	}
+}
+
+func Shutdown() {
+	conn.Close()
+}
+
+func Publish(ctx context.Context, b []byte) error {
+	ch, err := conn.Channel()
+	if err != nil {
+		return err
+	}
+
+	q, err := ch.QueueDeclare("text", false, false, false, false, nil)
+	if err != nil {
+		return err
+	}
+
+	if err := ch.PublishWithContext(ctx,
+		"",
+		q.Name,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "application/json",
+			Body: b,
+		},
+	); err != nil {
+		return err
+	}
+	return nil
+}
--- main.go
+++ main.go
@@ -7,18 +7,12 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	amqp "github.com/rabbitmq/amqp091-go"
+
+	"github.com/stuff-ai/api/internal/rmq"
 )
 
-var conn *amqp.Connection
-
 func main() {
-	// Rabbit
-	var err error
-	if conn, err = amqp.Dial("amqp://guest:guest@192.168.63.29:5672/"); err != nil {
-		panic("failled to initialize amqp: " + err.Error())
-	}
-	defer conn.Close()
+	defer rmq.Shutdown()
 
 	// Echo instance
 	e := echo.New()
@@ -45,39 +39,12 @@ func generate(c echo.Context) error {
 	if err := c.Bind(req); err != nil {
 		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
-	if err := publish(req); err != nil {
+	b, err := json.Marshal(req)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	if err := rmq.Publish(context.Background(), b); err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
 	}
   	return c.String(http.StatusOK, "OK")
-}
-
-func publish(req *Request) error {
-	ch, err := conn.Channel()
-	if err != nil {
-		return err
-	}
-
-	q, err := ch.QueueDeclare("text", false, false, false, false, nil)
-	if err != nil {
-		return err
-	}
-
-	reqJSON, err := json.Marshal(req)
-	if err != nil {
-		return err
-	}
-
-	if err := ch.PublishWithContext(context.Background(),
-		"",
-		q.Name,
-		false,
-		false,
-		amqp.Publishing{
-			ContentType: "application/json",
-			Body: []byte(reqJSON),
-		},
-	); err != nil {
-		return err
-	}
-	return nil
 }
